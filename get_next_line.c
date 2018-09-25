d1a4f65d4f1112c810034e92db754c244e313a51
mguerrea <mguerrea@student.42.fr>
2018-09-25 19:36:46 +0100

    gestion de la valeur de retour de la fonction

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mguerrea <mguerrea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/23 16:28:34 by mguerrea          #+#    #+#             */
-/*   Updated: 2018/09/23 19:43:51 by mguerrea         ###   ########.fr       */
+/*   Updated: 2018/09/25 19:35:53 by mguerrea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,6 @@
 #include <stdio.h>
 
 #define BUFF_SIZE 4
-
-void    ft_lstaddback(t_list **list, t_list *new)
-{
-    t_list *temp;
-
-    temp = *list;
-    if (!temp)
-    {
-        *list = new;
-        return ;
-    }
-    while (temp->next)
-        temp = temp->next;
-    temp->next = new;
-}
 
 int ft_checkline(char *str)
 {
@@ -57,15 +42,12 @@ char    *ft_cpy_line(char *dst, char *src, char *perm)
     i = 0;
     while (src[i] != '\n' && src[i])
         i++;
-//    printf("b6\n");
     len = i;
     if (perm)
         len = i + ft_strlen(perm);
     dst = ft_strnew(len);
-//    printf("b7\n");
     if (perm)
         dst = ft_strcpy(dst, perm);
-//    printf("b8\n");
     dst = ft_strncat(dst, src, i);
     return (dst);
 }
@@ -88,28 +70,23 @@ int get_next_line(int fd, char **line)
     int ret;
     char buf[BUFF_SIZE + 1];
     t_list *list;
-    t_list *new;
     static char *perm = NULL;
     char *str;
 
     list = NULL;
-//    printf("b1\n");
     while ((ret = read(fd, buf, BUFF_SIZE)))
     {
         buf[ret] = '\0';
-//        printf("b2\n");
-        new = ft_lstnew(buf, ret + 1);
-//        printf("b3\n");
-        ft_lstaddback(&list, new);
-//        printf("b4\n");
+        ft_lstaddback(&list, ft_lstnew(buf, ret + 1));
         if (ft_checkline(buf))
             break ;
     }
+    if (ret == -1)
+        return (-1);
     ft_list_to_str(list, &str);
-//    printf("b5\n");
     *line = ft_cpy_line(*line, str, perm);
     perm = ft_save(str);
-    return (0);
+    return ((ret < BUFF_SIZE) ? 0 : 1);
 }
 
 int main(int argc, char **argv)
@@ -125,5 +102,6 @@ int main(int argc, char **argv)
     i = get_next_line(fd, &line);
     ft_putstr(line);
     ft_putchar('\n');
+    printf("%d\n", i);
     return(0);
 }
